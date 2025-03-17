ef02da54b5ab5d99ea4c2279dd744dc003588d29
ShivaPrasadDevanaboyina <devanaboyina@cua.edu>
2025-03-17 18:52:42 -0400

    data/content.js

---
+++ content.js
@@ -0,0 +1,22 @@
+
+const contentData = {
+  title: "Top Countries to Visit",
+  description: "Explore the most breathtaking places to visit around the globe.",
+  items: [
+    {
+      heading: "USA",
+      text: "Explore the Grand Canyon, New York City, and more.",
+      image: "images/one.jpg"
+    },
+    {
+      heading: "Japan",
+      text: "Visit Mount Fuji, Kyoto, and the bustling streets of Tokyo.",
+      image: "images/two.jpg.webp"
+    },
+    {
+      heading: "Italy",
+      text: "See the Colosseum, Venice canals, and the Leaning Tower of Pisa.",
+      image: "images/three.jpg"
+    }
+  ]
+};
